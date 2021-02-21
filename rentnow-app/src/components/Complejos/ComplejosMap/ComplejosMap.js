af6761477ccdde7db777bd6b8f83f96a245c2987
Sebastian Magnaldi <sebastian@munidigital.com>
2021-02-21 16:33:24 -0300

    Elimino import sin uso

--- rentnow-app/src/components/Complejos/ComplejosMap/ComplejosMap.js
+++ rentnow-app/src/components/Complejos/ComplejosMap/ComplejosMap.js
@@ -1,9 +1,7 @@
-import React, { useState, useCallback, useEffect } from 'react'
-import { GoogleMap, LoadScript, OverlayView } from "@react-google-maps/api";
+import React from 'react'
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import { GOOGLE_MAP_KEY } from "constants/apiKeys";
 import { Marker } from "@react-google-maps/api";
-
-
 
 const containerStyle = {
     width: '100%',
@@ -15,22 +13,9 @@ const center = {
     lng: -38.523
 };
 
-const contentStyles = {
-    background: `white`,
-    border: `1px solid #CCC`,
-    padding: 15,
-}
-
-const centerOverlayView = (width, height) => ({
-    x: -(width / 2),
-    y: -(height / 2),
-})
-
-
 const ComplejosMap = ({complejos}) => {
 
     return (
-
         <LoadScript
             googleMapsApiKey={GOOGLE_MAP_KEY}
         >
@@ -42,10 +27,7 @@ const ComplejosMap = ({complejos}) => {
                 {complejos.map((complejo) => <Marker position={{ lat: complejo.ubicacion.latlng.latitude, lng: complejo.ubicacion.latlng.longitude }}  />)}
             </GoogleMap>
         </LoadScript>
-
     )
 }
-
-//CAMBIAR MARKERS POR LOGO DE RENTNOW.
 
 export default ComplejosMap
