452515dba2240eb5f983cca02b2bbeae1f42ab7b
facuvillard <facundo.villard@mercadolibre.com>
2021-03-19 16:07:10 -0300

    Primera aproximacion al diseño de vista de complejos

--- rentnow-app/src/components/Complejos/ComplejoInfoWindow/ComplejoInfoWindow.js
+++ rentnow-app/src/components/Complejos/ComplejoInfoWindow/ComplejoInfoWindow.js
@@ -1,16 +1,60 @@
-import React from 'react' 
-import {InfoWindow} from "@react-google-maps/api"
+import React from 'react';
+import { InfoWindow } from '@react-google-maps/api';
+import { Card, CardHeader, CardMedia, Avatar } from '@material-ui/core';
+
+import logo from "assets/Landing/logo-amarillo-simple.png"
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		maxWidth: 345,
+	},
+	media: {
+		height: 0,
+		paddingTop: '56.25%',
+	},
+    avatar : {
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+        backgroundColor: theme.palette.secondary.dark,
+    } 
+}));
 
 export default function ComplejoInfoWindow(props) {
-    const {complejo} = props;
-    const position = { lat: complejo.ubicacion.latlng.latitude, lng: complejo.ubicacion.latlng.longitude }
+	const { complejo } = props;
+	const classes = useStyles();
+	const position = {
+		lat: complejo.ubicacion.latlng.latitude,
+		lng: complejo.ubicacion.latlng.longitude,
+	};
 
-    const onCloseClickHandler = () => {
-        props.setComplejo(null);
-    }
+	console.log(complejo);
 
-    return (
-    <InfoWindow onCloseClick={onCloseClickHandler} position={position} >
-        <h1>Info del complejo</h1>
-    </InfoWindow>)
+	const onCloseClickHandler = () => {
+		props.setComplejo(null);
+	};
+	return (
+		<InfoWindow onCloseClick={onCloseClickHandler} position={position}>
+			<Card className={classes.root}>
+				<CardHeader
+					avatar={
+						<Avatar className={classes.avatar} src={logo}  />
+
+					}
+					titleTypographyProps={{variant: "h5"} }
+					subheaderTypographyProps="h3"
+					title={complejo.nombre}
+					subheader={
+						complejo.ubicacion.calle +
+						' ' +
+						complejo.ubicacion.numero +
+						', ' +
+						complejo.ubicacion.ciudad +
+						'.'
+					}
+				/>
+				<CardMedia className={classes.media} image={complejo.fotos[0]} title="Imagen complejo" />
+			</Card>
+		</InfoWindow>
+	);
 }
