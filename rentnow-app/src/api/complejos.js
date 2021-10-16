8053c74136bfca20bf5236a8165e4b2ca929ae4b
juanpbergues <32652663+juanpbergues@users.noreply.github.com>
2021-10-16 18:15:53 -0300

    Listado finalizado

--- rentnow-app/src/api/complejos.js
+++ rentnow-app/src/api/complejos.js
@@ -1,126 +1,170 @@
+import { Complejos } from "components/Complejos/Complejos";
 import firebase from "firebase";
 import * as geofire from "geofire-common";
 
 export async function getComplejosById(id) {
-    try {
-      const result = await firebase
-        .firestore()
-        .collection("complejos")
-        .doc(id)
-        .get();
-  
-      const complejo = { id: result.id, ...result.data() };
-      return {
-        status: "OK",
-        message: "Se consultaron los complejos con exito",
-        data: complejo,
-      };
-    } catch (err) {
-      console.log(err);
-      return {
-        status: "ERROR",
-        message: "Se produjo un error al consultar los complejos",
-        error: err,
-      };
-    }
-  }
+  try {
+    const result = await firebase
+      .firestore()
+      .collection("complejos")
+      .doc(id)
+      .get();
 
-  export async function getComplejosEnabledApi() {
-    try {
-      const result = await firebase
-        .firestore()
-        .collection("complejos")
-        .where("habilitado", "==", true)
-        .orderBy("fechaAlta", "asc")
-        .get();
-      const complejos = result.docs.map((complejo) => {
-        const complejoData = complejo.data();
-        return {
-          id: complejo.id,
-          ...complejoData,
-        };
-      });
-      return {
-        status: "OK",
-        message: "Se consultaron los complejos con exito",
-        data: complejos,
-      };
-    } catch (err) {
-      return {
-        status: "ERROR",
-        message: "Se produjo un error al consultar los complejos",
-        error: err,
-      };
-    }
+    const complejo = { id: result.id, ...result.data() };
+    return {
+      status: "OK",
+      message: "Se consultaron los complejos con exito",
+      data: complejo,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      status: "ERROR",
+      message: "Se produjo un error al consultar los complejos",
+      error: err,
+    };
   }
-  
-  export async function getNearbyComplejos(center, radius) {
-    try {
-      const bounds = geofire.geohashQueryBounds(center, radius);
-      const promises = [];
-  
-      bounds.forEach(b => {
-        const q = firebase.firestore()
-          .collection("complejos")
-          .orderBy("ubicacion.geohash")
-          .startAt(b[0])
-          .endAt(b[1]);
-  
-        promises.push(q.get());
-      })
-  
-      let snapshots = await Promise.all(promises);
-      const matchingDocs = []
-      snapshots.forEach(snap =>
-        snap.forEach(doc => {
-          const lat = doc.get('ubicacion').latlng.latitude;
-          const lng = doc.get('ubicacion').latlng.longitude;
-          const distanceInKm = geofire.distanceBetween([lat, lng], center);
-          const distanceInM = distanceInKm * 1000;
-  
-          if (distanceInM <= radius) {
-            matchingDocs.push(doc);
-          }
-  
-        }))
-  
-        let complejos = await matchingDocs.map( doc => {return {id: doc.id, ...doc.data()}})
-  
-      return {
-        status: "OK",
-        message: "Se consultaron los complejos con exito",
-        data: complejos,
-      };
-    } catch (err) {
-      console.log(err)
-      return {
-        status: "ERROR",
-        message: "Se produjo un error al consultar los complejos",
-        error: err,
-      };
-    }
-  }
+}
 
-  export async function getComplejoNameImagesAndUbicacion(id) {
-    try {
-      const result = await firebase
-        .firestore()
-        .collection("complejos")
-        .doc(id)
-        .get()
-
-      const complejo = result.data() ;
+export async function getComplejosEnabledApi() {
+  try {
+    const result = await firebase
+      .firestore()
+      .collection("complejos")
+      .where("habilitado", "==", true)
+      .orderBy("fechaAlta", "asc")
+      .get();
+    const complejos = result.docs.map((complejo) => {
+      const complejoData = complejo.data();
       return {
-        status: "OK",
-        message: "Se consulto las imagenes del complejo con exito",
-        data: { fotos: complejo.fotos, nombre: complejo.nombre, ubicacion: complejo.ubicacion },
+        id: complejo.id,
+        ...complejoData,
       };
-    } catch (err) {
-      console.log(err);
-      return {
-        status: "ERROR",
-        message: "Se produjo un error al consultar las imagenes del complejo",
-        error: err,
-      };
-    }
+    });
+    return {
+      status: "OK",
+      message: "Se consultaron los complejos con exito",
+      data: complejos,
+    };
+  } catch (err) {
+    return {
+      status: "ERROR",
+      message: "Se produjo un error al consultar los complejos",
+      error: err,
+    };
   }
+}
+
+export async function getNearbyComplejos(center, radius) {
+  try {
+    const bounds = geofire.geohashQueryBounds(center, radius);
+    const promises = [];
+
+    bounds.forEach(b => {
+      const q = firebase.firestore()
+        .collection("complejos")
+        .orderBy("ubicacion.geohash")
+        .startAt(b[0])
+        .endAt(b[1]);
+
+      promises.push(q.get());
+    })
+
+    let snapshots = await Promise.all(promises);
+    const matchingDocs = []
+    snapshots.forEach(snap =>
+      snap.forEach(doc => {
+        const lat = doc.get('ubicacion').latlng.latitude;
+        const lng = doc.get('ubicacion').latlng.longitude;
+        const distanceInKm = geofire.distanceBetween([lat, lng], center);
+        const distanceInM = distanceInKm * 1000;
+
+        if (distanceInM <= radius) {
+          matchingDocs.push(doc);
+        }
+
+      }))
+
+    let complejos = await matchingDocs.map(doc => { return { id: doc.id, ...doc.data() } })
+
+    return {
+      status: "OK",
+      message: "Se consultaron los complejos con exito",
+      data: complejos,
+    };
+  } catch (err) {
+    console.log(err)
+    return {
+      status: "ERROR",
+      message: "Se produjo un error al consultar los complejos",
+      error: err,
+    };
+  }
+}
+
+export async function getComplejoNameImagesAndUbicacion(id) {
+  try {
+    const result = await firebase
+      .firestore()
+      .collection("complejos")
+      .doc(id)
+      .get()
+
+    const complejo = result.data();
+    return {
+      status: "OK",
+      message: "Se consulto las imagenes del complejo con exito",
+      data: { fotos: complejo.fotos, nombre: complejo.nombre, ubicacion: complejo.ubicacion },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      status: "ERROR",
+      message: "Se produjo un error al consultar las imagenes del complejo",
+      error: err,
+    };
+  }
+}
+
+export async function getComplejosByFilters(filtros) {
+  try {
+    const { ciudad, tipoEspacio, provincia } = filtros;
+    const hasFilters = tipoEspacio !== "Todos";
+
+    const result = await firebase
+      .firestore()
+      .collection("complejos")
+      .where("habilitado", "==", true)
+      .where("ubicacion.provincia", "==", provincia)
+      .where("ubicacion.ciudad", "==", ciudad)
+      .get();
+
+    const complejos = result.docs.map((complejoRef) => ({
+      id: complejoRef.id,
+      ...complejoRef.data(),
+    }));
+
+    const checkFilters = (complejo) => {
+      const { espaciosMetaData: espacios } = complejo;
+
+      return espacios.some((espacio) => espacio.tipoEspacio === tipoEspacio);
+    };
+
+    const filteredComplejos = hasFilters
+      ? complejos.filter(checkFilters)
+      : complejos;
+
+    return {
+      status: "OK",
+      message: "Se consultaron los complejos con exito",
+      data: filteredComplejos,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      status: "ERROR",
+      message: "Se produjo un error al consultar los complejos",
+      error: err,
+    };
+  }
+}
--- rentnow-app/src/components/Complejos/ComplejosList/ComplejosList.js
+++ rentnow-app/src/components/Complejos/ComplejosList/ComplejosList.js
@@ -1,26 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import {
-	List,
-	ListItem,
-	CardActionArea,
-	Card,
-	CardMedia,
-	CardContent,
-	Typography,
-	InputLabel,
-	Select,
-	MenuItem,
-	FormControl,
-	Grid,
-	Chip
+	List, ListItem, CardActionArea,
+	Card, CardMedia, CardContent,
+	Typography, ListItemText, Avatar,
+	Grid, Chip, IconButton, Divider,
+	TextField, MenuItem, Button, makeStyles
 } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom';
 import { Alert, AlertTitle } from '@material-ui/lab';
-
+import { Autocomplete } from '@material-ui/lab';
 import { tipoEspacio } from 'constants/espacios/tipoEspacio';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import NewReleasesIcon from '@material-ui/icons/NewReleases';
+import EspacioImage from 'assets/FilterButtons/basketball.png'
+import LocationImage from 'assets/FilterButtons/location.png'
+import DialogCustom from "components/utils/DialogCustom/DialogCustom"
+import { AuthContext } from 'Auth/Auth';
+import { getComplejosByFilters } from 'api/complejos';
+import { getProvincesApi, getCitiesByProvincesApi } from 'api/geoApi';
+import { useFormik } from 'formik';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -57,112 +55,326 @@ const useStyles = makeStyles((theme) => ({
 		margin: 'auto',
 		marginLeft: theme.spacing(2),
 	},
-	container:{
+	container: {
 		display: 'inline-flex'
 	},
 	chipValoracion: {
-        margin: 'auto',
+		margin: 'auto',
 		marginLeft: theme.spacing(2),
-    },
+	},
 	chipCancha: {
 		marginRight: theme.spacing(0.5),
 	},
+	divider: {
+		marginTop: theme.spacing(2),
+		marginBottom: theme.spacing(2),
+	},
+	ciudadContainer: {
+		marginBottom: theme.spacing(2),
+	},
+	ciudadSelect: {
+		width: '15em',
+		margin: theme.spacing(2)
+	},
+	filterChip: {
+		margin: theme.spacing(0.25)
+	}
 }));
 
-export const ComplejosList = ({ complejos }) => {
+function SelectTipoEspacio({ open, tiposEspacio, onClose, selectedTipo }) {
+	const classes = useStyles();
+
+	const handleClose = () => {
+		onClose(selectedTipo);
+	};
+
+	const handleListItemClick = (tipo) => {
+		onClose(tipo);
+	};
+
+	return (
+		<DialogCustom title="Tipos de espacio " open={open} onClose={handleClose}>
+			<Divider className={classes.divider} />
+			<List>
+				<ListItem autoFocus onClick={() => { handleListItemClick('Todos') }} button>
+					<ListItemText primary='Todos' />
+				</ListItem>
+				{
+
+					tiposEspacio.map((tipo, index) => {
+						return (
+							<ListItem autoFocus onClick={() => { handleListItemClick(tipo.value) }} button key={tipo + index}>
+								<ListItemText primary={tipo.value} />
+							</ListItem>
+						)
+					}
+					)}
+			</List>
+		</DialogCustom>
+	)
+}
+
+function SelectCiudad({ open, onClose, setProvinces, setCities, cities, provinces, selectedCiudad, selectedProvincia, setOpenSelectCiudad }) {
+	const classes = useStyles();
+
+	useEffect(() => {
+		getProvincesApi().then((response) => {
+			setProvinces(response.provincias);
+		});
+	}, [setProvinces]);
+
+	const handleCityTextField = async (e) => {
+		if (e.target.value.length > 3) {
+			await getCitiesByProvincesApi(formik.values.provincia, e.target.value).then((response) => {
+				setCities(response.localidades);
+			});
+		} else {
+			setCities([]);
+		}
+	};
+
+	const formik = useFormik({
+		initialValues: {
+			provincia: selectedProvincia,
+			ciudad: selectedCiudad,
+		},
+		onSubmit: (values) => {
+			onClose(values)
+		},
+	});
+
+	return (
+		<DialogCustom title="Provincia y Ciudad" open={open} onClose={() => setOpenSelectCiudad(false)}>
+			<Divider className={classes.divider} />
+			<form onSubmit={formik.handleSubmit}>
+				<Grid
+					container
+					direction="column"
+					justifyContent="center"
+					alignItems="center"
+					className={classes.ciudadContainer}
+				>
+					<Grid item xs={12}>
+						<TextField
+							select
+							variant="outlined"
+							margin="normal"
+							required
+							className={classes.ciudadSelect}
+							label="Provincia"
+							name="provincia"
+							autoComplete="provincia"
+							value={formik.values.provincia}
+							onChange={(e) => {
+								formik.handleChange(e);
+								formik.setFieldValue('ciudad', '');
+							}}
+						>
+							{provinces.map((opt) => (
+								<MenuItem key={opt.id} value={opt.nombre}>
+									{opt.nombre}
+								</MenuItem>
+							))}
+						</TextField>
+					</Grid>
+					<Grid item xs={12}>
+						<Autocomplete
+							disabled={formik.values.provincia ? false : true}
+							options={cities}
+							getOptionLabel={(option) => option.nombre}
+							inputValue={formik.values.ciudad}
+							renderInput={(params) => (
+								<TextField
+									className={classes.ciudadSelect}
+									{...params}
+									margin="normal"
+									name="ciudad"
+									label="Ciudad"
+									variant="outlined"
+									required
+									onChange={handleCityTextField}
+								/>
+							)}
+							onInputChange={(e, inputValue) => {
+								formik.setValues({ ...formik.values, ciudad: inputValue });
+							}}
+						/>
+					</Grid>
+					<Grid item xs={6}>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							color="primary"
+							className={classes.submit}
+						>
+							Aceptar
+						</Button>
+					</Grid>
+				</Grid>
+			</form>
+		</DialogCustom>
+	)
+}
+
+export const ComplejosList = () => {
+	const { currentUserData } = useContext(AuthContext);
 	const classes = useStyles();
 	const history = useHistory();
-	const [tipoEspacioSelected, setTipoEspacioSelected] = useState('Todos');
 	const [complejosList, setComplejosList] = useState([]);
+	const [selectedTipoEspacio, setSelectedTipoEspacio] = useState('Todos');
+	const [openSelectTipoEspacio, setOpenSelectTipoEspacio] = useState(false);
+	const [provinces, setProvinces] = useState([]);
+	const [cities, setCities] = useState([]);
+	const [selectedProvincia, setSelectedProvincia] = useState(currentUserData.provincia);
+	const [selectedCiudad, setSelectedCiudad] = useState(currentUserData.ciudad);
+	const [openSelectCiudad, setOpenSelectCiudad] = useState(false);
+
+	const handleSelectTipoEspacioOpen = () => {
+		setOpenSelectTipoEspacio(true)
+	}
+
+	const handleCloseSelectTipoEspacio = (value) => {
+		setOpenSelectTipoEspacio(false);
+		setSelectedTipoEspacio(value);
+	};
+
+	const handleSelectCiudadOpen = () => {
+		setOpenSelectCiudad(true)
+	}
+
+	const handleCloseSelectedCiudad = (value) => {
+		setOpenSelectCiudad(false);
+		setSelectedProvincia(value.provincia)
+		setSelectedCiudad(value.ciudad);
+	};
 
 	function goToComplejoDetail(idComplejo) {
 		history.push(`/complejos/${idComplejo}`);
 	}
 
 	useEffect(() => {
-		if (tipoEspacioSelected === 'Todos') {
-			setComplejosList(complejos);
-		} else {
-			const filtro = complejos.filter((com) =>
-				com.espaciosMetaData.some((esp) => esp.tipoEspacio === tipoEspacioSelected)
-			);
-			setComplejosList(filtro);
-		}
-	}, [tipoEspacioSelected]);
+		getComplejosByFilters({ tipoEspacio: selectedTipoEspacio, ciudad: selectedCiudad, provincia: selectedProvincia }).then(
+			result => {
+				setComplejosList(result.data)
+			}
+		);
+	}, [selectedCiudad, selectedProvincia, selectedTipoEspacio]);
 
 	return (
-		<List className={classes.list}>
-			<ListItem>
-				<FormControl className={classes.formControl}>
-					<InputLabel id="tipoEspacio">Tipo de espacio</InputLabel>
-					<Select
-						variant="outlined"
-						labelId="tipoEspacio"
-						id="tipoEspacioSelect"
-						value={tipoEspacioSelected}
-						onChange={(e) => setTipoEspacioSelected(e.target.value)}
-					>
-						<MenuItem value={'Todos'}>Todos</MenuItem>
-						{tipoEspacio.map((te) => (
-							<MenuItem value={te.value}>{te.key}</MenuItem>
-						))}
-					</Select>
-				</FormControl>
-			</ListItem>
-			{complejos.length === 0 ? (
-				<Grid container justify="center">
-					<Alert className={classes.AlertStyle} severity="warning">
-						<AlertTitle>No existen complejos en esta ubicación</AlertTitle>
-					</Alert>
-				</Grid>
-			) : complejosList.length === 0 ? (
-				<Grid container justify="center">
-					<Alert className={classes.AlertStyle} severity="warning">
-						<AlertTitle>No existen complejos con este tipo de espacio en esta ubicación</AlertTitle>
-					</Alert>
-				</Grid>
-			) : (
-				complejosList.map((complejo) => (
-					<ListItem key={complejo.id}>
-						<Card className={classes.root} elevation={4}>
-							<CardActionArea style={{ height: '100%' }} onClick={() => goToComplejoDetail(complejo.id)}>
-								<CardMedia className={classes.media} image={complejo.fotos[0]} title={complejo.nombre} />
-								<CardContent>
-									<Grid className={classes.container}>
-										<Typography gutterBottom variant="h6" component="h2">
-											{complejo.nombre}
-										</Typography>
-										{complejo.valoracion ? (
-											<Chip
-												icon={<StarBorderIcon />}
-												label={complejo.valoracion}
-												color="primary"
-												size='small'
-												className={classes.chipValoracion}
-											/>
-										) : (
-											<Chip
-												icon={<NewReleasesIcon style={{ color: '#FAFAFA' }} />}
-												label={'Nuevo'}
-												size='small'
-												className={classes.chipSuccess}
-											/>
-										)}
-									</Grid>
-									{complejo.espaciosMetaData.map((espacio) => (
-										<Chip
-											label={espacio.tipoEspacio}
-											color="secondary"
-											size='small'
-											className={classes.chipCancha}
-										/>
-									))}
-								</CardContent>
-							</CardActionArea>
-						</Card>
-					</ListItem>
-				))
-			)}
-		</List>
+		<>
+			<Grid>
+				<Typography variant="h6" gutterBottom>
+					Filtros
+				</Typography>
+			</Grid>
+			<Grid
+				direction="row"
+				justifyContent="space-evenly"
+				alignItems="center"
+			>
+				<div style={{ display: 'inline-grid', marginRight: '10px' }}>
+					<IconButton><img src={EspacioImage} alt="espacio" onClick={handleSelectTipoEspacioOpen} /></IconButton>
+					<Typography variant="subtitle2" gutterBottom>
+						Espacio
+					</Typography>
+				</div>
+				<div style={{ display: 'inline-grid', marginRight: '10px' }}>
+					<IconButton><img src={LocationImage} alt="ciudad" onClick={handleSelectCiudadOpen} /></IconButton>
+					<Typography variant="subtitle2" gutterBottom>
+						Ciudad
+					</Typography>
+				</div>
+			</Grid>
+			<Divider className={classes.divider} />
+			<Grid
+				container
+				direction="row"
+				justifyContent="flex-start"
+				alignItems="center"
+				spacing={2}
+			>
+				<Chip className={classes.filterChip} color="primary" avatar={<Avatar src={EspacioImage} />} label={`Tipo de Espacio: ${selectedTipoEspacio}`} />
+				<Chip className={classes.filterChip}  color="primary" avatar={<Avatar src={LocationImage} />} label={`Ciudad: ${selectedCiudad}`} />
+			</Grid>
+			<List className={classes.list}>
+				{complejosList.length === 0 ? (
+					<Grid container justify="center">
+						<Alert className={classes.AlertStyle} severity="warning">
+							<AlertTitle>No existen complejos con este tipo de espacio en esta ubicación</AlertTitle>
+						</Alert>
+					</Grid>
+				) : (
+					complejosList.map((complejo) => {
+						let tiposEspacios = []
+						return (
+							<ListItem key={complejo.id}>
+								<Card className={classes.root} elevation={4}>
+									<CardActionArea style={{ height: '100%' }} onClick={() => goToComplejoDetail(complejo.id)}>
+										<CardMedia className={classes.media} image={complejo.fotos[0]} title={complejo.nombre} />
+										<CardContent>
+											<Grid className={classes.container}>
+												<Typography gutterBottom variant="h6" component="h2">
+													{complejo.nombre}
+												</Typography>
+												{complejo.valoracion ? (
+													<Chip
+														icon={<StarBorderIcon />}
+														label={complejo.valoracion}
+														color="primary"
+														size='small'
+														className={classes.chipValoracion}
+													/>
+												) : (
+													<Chip
+														icon={<NewReleasesIcon style={{ color: '#FAFAFA' }} />}
+														label={'Nuevo'}
+														size='small'
+														className={classes.chipSuccess}
+													/>
+												)}
+											</Grid>
+											{complejo.espaciosMetaData.map((espacio) => {
+												if (tiposEspacios.includes(espacio.tipoEspacio)) {
+													return null
+												}
+												else {
+													tiposEspacios.push(espacio.tipoEspacio)
+													return (
+														<Chip
+															label={espacio.tipoEspacio}
+															color="secondary"
+															size='small'
+															className={classes.chipCancha}
+														/>
+													)
+												}
+											})}
+										</CardContent>
+									</CardActionArea>
+								</Card>
+							</ListItem>
+						)
+					})
+				)}
+			</List>
+			<SelectTipoEspacio
+				open={openSelectTipoEspacio}
+				tiposEspacio={tipoEspacio}
+				onClose={handleCloseSelectTipoEspacio}
+				selectedTipo={selectedTipoEspacio}
+			/>
+			<SelectCiudad
+				open={openSelectCiudad}
+				cities={cities}
+				setCities={setCities}
+				provinces={provinces}
+				setProvinces={setProvinces}
+				onClose={handleCloseSelectedCiudad}
+				selectedCiudad={selectedCiudad}
+				selectedProvincia={selectedProvincia}
+				setOpenSelectCiudad={setOpenSelectCiudad}
+			/>
+		</>
 	);
 };
